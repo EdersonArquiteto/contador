efa688d9ca23fa654481b84892f8450ed8f54e2d
ederson.silva <edersonarquiteto.net@gmail.com>
2024-08-30 15:40:29 -0300

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import {ref} from "vue"
+
+let count = ref(0)
+</script>
+
+<template>
+  <main>
+      <container class="container">
+        <h4>O valor do contador é...</h4>
+        <h1>{{count}}</h1>
+        <button @click="count--">-</button>
+        <button @click="count++">+</button>
+      </container>
+  </main>
+</template>
+
+<style scoped>
+main {
+  width: 100vw;
+  height: 100vh;
+  background-color: aliceblue;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.container {
+  text-align: center;
+}
+</style>
